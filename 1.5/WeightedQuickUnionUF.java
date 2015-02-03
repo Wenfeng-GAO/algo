5d294be75f7c1b0aff7eb88bfc1780b934f65031
wenfeng <elricfeng@gmail.com>
2015-02-03 05:32:05 +0100

    update

--- 1.5/WeightedQuickUnionUF.java
+++ 1.5/WeightedQuickUnionUF.java
@@ -1,13 +1,17 @@
-public class UF {
+public class WeightedQuickUnionUF {
     private int[] id; // access to comonent id (site indexed)
+    private int[] sz; // size of component for roots (site indexed)
     private int count; // number of components
 
-    public UF(int N) {
+    public WeightedQuickUnionUF(int N) {
         // Initialize component id array.
         count = N;
         id = new int[N];
         for (int i = 0; i < N; i++)
             id[i] = i;
+        sz = new int[N];
+        for (int i = 0; i < N; i++)
+            sz[i] = 1;
     }
 
     public int count() { return count; }
@@ -30,43 +34,28 @@ public class UF {
     }
 
     /**
-     * 1. Quick-find algo
-     */
-    /*
-    public int find(int p) { return id[p]; }
-
-    public void union(int p, int q) {
-        // Put p and q into the same component.
-        int pId = find(p);
-        int qId = find(q);
-
-        // Nothing to do if p and q are already in the same component.
-        if (pId == qId) return;
-
-        // Rename p's component to q's name.
-        for (int i = 0; i < id.length; i++) 
-            if (id[i] == pId) id[i] = qId;
-        count--;
-    }
-    */
-
-    /**
-     * 2. Quick-union algo
+     * 2. Weighted Quick-union algo
      */
     private int find(int p) {
-        // Find component name.
+        // Follow links to find a root.
         while (p != id[p]) p = id[p];
         return p;
     }
 
     public void union(int p, int q) {
         // Give p and q the same root.
-        int pRoot = find(p);
-        int qRoot = find(q);
-        if (pRoot == qRoot) return;
+        int i = find(p);
+        int j = find(q);
+        if (i == j) return;
 
-        id[pRoot] = qRoot;
-
+        // Make smaller root point to larger one.
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
         count--;
     }
 }
